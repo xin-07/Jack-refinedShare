130a80a2462255d704ad41f09cbf3f4d58c72905
xiny <2074835619@qq.com>
2026-07-29 17:04:04 +0800

    更新多个栏目首页描述，重命名Jack商业启蒙文档，完善侧边栏

--- docs/Jack商业启蒙/sidebar.ts
+++ docs/Jack商业启蒙/sidebar.ts
@@ -2,7 +2,17 @@ import type { DefaultTheme } from 'vitepress'
 
 const sidebarBusiness: DefaultTheme.SidebarGroup = {
   text: 'Jack商业启蒙',
-  items: [{ text: '麦当劳', link: '/Jack商业启蒙/docs/Jack商业启蒙——麦当劳' }],
+  items: [
+    { text: '麦当劳', link: '/Jack商业启蒙/docs/Jack商业启蒙——麦当劳' },
+    { text: '开市客', link: '/Jack商业启蒙/docs/Jack商业启蒙——开市客' },
+    { text: 'SpaceX', link: '/Jack商业启蒙/docs/Jack商业启蒙——SpaceX' },
+    { text: '泡泡玛特', link: '/Jack商业启蒙/docs/Jack商业启蒙——泡泡玛特' },
+    { text: '美光', link: '/Jack商业启蒙/docs/Jack商业启蒙——美光' },
+    { text: 'Nike', link: '/Jack商业启蒙/docs/Jack商业启蒙——Nike' },
+    { text: 'Sell Put', link: '/Jack商业启蒙/docs/Jack商业启蒙——Sell Put' },
+    { text: 'QQQ', link: '/Jack商业启蒙/docs/Jack商业启蒙——QQQ' },
+    { text: '苹果', link: '/Jack商业启蒙/docs/Jack商业启蒙——苹果' },
+  ],
 }
 
 export default sidebarBusiness
